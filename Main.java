3f57f3e5a29e1617a27a6976c52f1a7cfe18f2d0
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-03 09:15:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+public String name,address,cnum,email,ptype,pid;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     Main a=new Main();
+		Scanner sc=new Scanner(System.in);
+		String name,address,cnum,email,ptype,pid;
+		System.out.println("Enter your name:");
+		a.name=sc.nextLine();
+		System.out.println("Enter your address:");
+		a.address=sc.nextLine();
+		System.out.println("Enter your contact number:");
+		a.cnum=sc.nextLine();
+		System.out.println("Enter your email:");
+		a.email=sc.nextLine();
+		System.out.println("Enter proof type:");
+		a.ptype=sc.nextLine();
+		System.out.println("Enter proof id:");
+		a.pid=sc.nextLine();
+		System.out.println("Your details are:\n");
+		//a.display(name,address,cnum,email,ptype,pid);
+		System.out.println("Name:"+a.name);
+		System.out.println("Address:"+a.address);
+		System.out.println("Contact No:"+a.cnum);
+		System.out.println("Email:"+a.email);
+		System.out.println("Proof type:"+a.ptype);
+		System.out.println("Proof ID:"+a.pid);
+		
+		
+	}
+
+	//public String name;
+
+}
