4bad635e80b39a3463f9962248e1c0d6e3a6d341
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-04 09:30:26 +0530

    Add files via upload

---
+++ Abstraction.java
@@ -0,0 +1,40 @@
+
+public class Abstraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Racoon racoon=new Racoon("Racoon");
+        racoon.eat();
+        racoon.breathe();
+	}
+
+}
+abstract class Animal
+{
+	private String name;
+	public Animal(String name)
+	{
+		this.name=name;
+	}
+	public abstract void eat();
+	public abstract void breathe();
+	public String getName()
+	{
+		return name;
+	}
+}
+class Racoon extends Animal
+{
+	public Racoon(String name)
+	{
+		super(name);
+	}public void eat()
+	{
+	System.out.println(getName()+"is eating");	
+	}
+	public void breathe()
+	{
+		System.out.println("Breath in, breathe out, repeat");
+	}
+	
+}
---
+++ Encapsulation.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		Scanner sc=new Scanner(System.in);
+		String name,address,cnum,email,ptype,pid;
+		System.out.println("Enter your name:");
+		name=sc.nextLine();
+		System.out.println("Enter your address:");
+		address=sc.nextLine();
+		System.out.println("Enter your contact number:");
+		cnum=sc.nextLine();
+		System.out.println("Enter your email:");
+		email=sc.nextLine();
+		System.out.println("Enter proof type:");
+		ptype=sc.nextLine();
+		System.out.println("Enter proof id:");
+		pid=sc.nextLine();
+		Getset d=new Getset();
+		d.setname(name,address,cnum,email,ptype,pid);
+		System.out.println("Name:"+d.getname());
+		System.out.println("Address:"+d.getadd());
+		System.out.println("Contact No:"+d.getcnum());
+		System.out.println("Email:"+d.getemail());
+		System.out.println("Proof type:"+d.getptype());
+		System.out.println("Proof ID:"+d.getpid());
+        }
+}
+class Getset
+{
+	String name,address,cnum,email,ptype,pid;
+	public void setname(String name,String address,String cnum,String email,String ptype,String pid)
+	{
+		this.name=name;
+		this.address=address;
+		this.cnum=cnum;
+		this.email=email;
+		this.ptype=ptype;
+		this.pid=pid;
+	}
+	public String getname()
+	{
+		return name;
+	}
+	public String getadd()
+	{
+		return address;
+	}
+	public String getcnum()
+	{
+		return cnum;
+	}
+	public String getemail()
+	{
+		return email;
+	}
+	public String getptype()
+	{
+		return ptype;
+	}
+	public String getpid()
+	{
+		return pid;
+	}
+
+}
+
