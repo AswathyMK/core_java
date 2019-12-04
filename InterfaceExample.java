2dae1fc95a08b7baf55374793453ef2d67cdc622
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-04 14:10:06 +0530

    Add files via upload

---
+++ InterfaceExample.java
@@ -0,0 +1,42 @@
+
+interface Example
+{
+	void method1();
+	void method2();
+	void method3();
+}
+
+
+
+
+ class InterfaceExample implements Example
+ {
+
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+	 
+	public void method2()
+	{
+		System.out.println("method2");
+	}
+	public void method3()
+	{
+		System.out.println("method3");
+	}
+	 
+	 
+	 
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Example t=new InterfaceExample();
+		t.method1();
+		t.method2();
+		t.method3();
+
+	}
+
+}
