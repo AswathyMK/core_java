7b55058d9eb80a50f57bfaf59bb2ab7a9a94c343
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-18 11:52:13 +0530

    Update StringIntern.java

--- StringIntern.java
+++ StringIntern.java
@@ -1,18 +1,15 @@
 
-public class StringIntern {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class StringIntern 
+{
+	public static void main(String[] args) 
+	{
 		String a="well";
 		String ab="All is "+a;
 		String abc="All is well";
 		String abcd="All is well";
 		System.out.println(abc.hashCode());
-		
 		System.out.println(ab.hashCode());
-		//System.out.println(abc==ab.intern());
 		System.out.println(abc==ab);
-		
 		System.out.println(abcd.hashCode());
 
 	}
