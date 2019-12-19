bc25dd4ae8e9a35beaaead3252a6a116177d17d2
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-19 15:56:38 +0530

    Program using String concat()

--- ConcatExample2.java
+++ ConcatExample2.java
@@ -1,12 +1,12 @@
 
-public class ConcatExample2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ConcatExample2 
+{
+	public static void main(String[] args)
+	{
 		String s1="Hello";
 		String s2="FAE@2.0";
 		String s3="Reader";
-		//concatenation one string
+		//concatenation of single string
 		String s4=s1.concat(s2);
 		System.out.println(s4);
 		//concatenating multiple strings
