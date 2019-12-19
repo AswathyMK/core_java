27b396c36a888aeba5374fd5ba4819f6ccd66ffb
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-19 12:02:21 +0530

    Program using  String endsWith()

---
+++ EndsWithExample2.java
@@ -0,0 +1,16 @@
+
+public class EndsWithExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Welcome to FACE.in";
+		System.out.println(str.endsWith("in"));
+		if(str.endsWith(".com"))
+		{
+			System.out.println("String ends with .com");
+		}else
+			System.out.println("It does not end with .com");
+
+	}
+
+}
