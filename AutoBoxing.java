7e125e003edbba75149bde40e0fe5221fb86818f
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-19 15:45:26 +0530

    Program for Autoboxing

--- AutoBoxing.java
+++ AutoBoxing.java
@@ -1,8 +1,8 @@
 
-public class AutoBoxing {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class AutoBoxing
+{
+	public static void main(String[] args) 
+	{
 		Integer i=20;//converting int into integer
 		Integer j=i;//autoboxing ,now compiler will write Intege.valueOf(a) internally
 		System.out.println(i+" "+j);
