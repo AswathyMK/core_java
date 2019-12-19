38a28882659cc32da48d557281d5ae4decc5eb3f
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-19 15:42:49 +0530

    Program using Anonymous class

--- AnonymousClass.java
+++ AnonymousClass.java
@@ -1,29 +1,22 @@
 
 //anonymous class example
-
-
 abstract class Person
 {
 	abstract void learn();
 	
 }
-
-
-
-public class AnonymousClass {
-
-
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class AnonymousClass
+{
+	public static void main(String[] args) 
+	{
 		Person obj=new Person()
 		{
-		public void learn()
-		{
-			System.out.print("HAI");
-		}
+			public void learn()
+			{
+				System.out.print("HAI");
+			}
 		};
-		//AnonymousClass.ob=new AnonymousClass();
+		
 		obj.learn();
 
 	}
