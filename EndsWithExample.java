3603662c16a5e7fa2738da0fb0bf370b558be79d
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-19 11:52:28 +0530

    Add files via upload

---
+++ EndsWithExample.java
@@ -0,0 +1,12 @@
+
+public class EndsWithExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="java by FACE";
+		System.out.println(s1.endsWith("E"));
+		System.out.println(s1.endsWith("FACE"));
+
+	}
+
+}
