cd39b2b04c7ea7cedd5f25f34a4be1bdafb55307
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-19 16:00:31 +0530

    Program using String contains()

--- ContainsExample.java
+++ ContainsExample.java
@@ -1,13 +1,12 @@
 
-public class ContainsExample {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class ContainsExample 
+{
+	public static void main(String[] args) 
+	{
 		String name="What do you know about me";
-		System.out.println(name.contains("do you know"));//print true value.
-		System.out.println(name.contains("about"));//print true value.
-		System.out.println(name.contains("hello"));//print false value.
+		System.out.println(name.contains("do you know")); //print true value.
+		System.out.println(name.contains("about")); //print true value.
+		System.out.println(name.contains("hello")); //print false value.
 	}
 
 }
