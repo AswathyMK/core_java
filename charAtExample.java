b376ef51d59a762f06fc68ed9b5dd3b512393b78
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 09:23:04 +0530

    Example program for charAt()

--- charAtExample.java
+++ charAtExample.java
@@ -1,13 +1,9 @@
-
-public class charAtExample {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class charAtExample 
+{
+	public static void main(String[] args) 
+	{
 		String name="FACE@2.0";
 		char ch=name.charAt(4);//returns the char value at the 4th index.
 		System.out.println(ch);//It print the value in 4th index.  
-		
 	}
-
 }
