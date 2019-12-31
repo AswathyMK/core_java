3040796d1ed9ecc42738cb93e8dd7f3c19ead33c
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 09:49:15 +0530

    Example program for exception handling

--- ExceptionHandling_Example2.java
+++ ExceptionHandling_Example2.java
@@ -2,9 +2,10 @@ class Test extends Exception
 {
 	
 }
-public class ExceptionHandling_Example2 {
-
-	public static void main(String[] args) {
+public class ExceptionHandling_Example2 
+{
+	public static void main(String[] args) 
+	{
 		try
 		{
 			throw new Test();
@@ -13,6 +14,7 @@ public class ExceptionHandling_Example2 {
 		{
 			System.out.println("Got the Test Exception");
 		}
+		//finally always excecutes whether there is any exception occures or not
 		finally
 		{
 			System.out.println("Inside finally block");
