197e6dc05127d1ac83dd0c6273c82c06d62acda0
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 09:52:58 +0530

    Program using String charAt()

--- charAtExample2.java
+++ charAtExample2.java
@@ -1,13 +1,10 @@
-
-public class charAtExample2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class charAtExample2 
+{
+	public static void main(String[] args) 
+	{
 		String name="FACE@2.0";
 		char ch=name.charAt(10);//returns the char value at the 10th index.
 		System.out.println(ch);//It shows StringIndexOutOfBoundException. 
-		
-
 	}
 
 }
