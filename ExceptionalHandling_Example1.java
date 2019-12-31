d521472f5c4281aef87fc5d5272dba00e1b5926a
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 09:39:19 +0530

    Example program for exception handling

--- ExceptionalHandling_Example1.java
+++ ExceptionalHandling_Example1.java
@@ -1,24 +1,21 @@
-
-public class ExceptionalHandling_Example1 {
-	static void check()throws ArithmeticException
+public class ExceptionalHandling_Example1 
+{
+	static void check()throws ArithmeticException//throws keyword used to show that which exception are to be handled
 	{
 		System.out.println("Inside check function");
 		throw new ArithmeticException("FACE Kollam");
 	}
-	
-
-	public static void main(String[] args) {
-		try
+	public static void main(String[] args) 
+	{
+		//try catch block is used for exception handling
+		try//Exception making statements are written in try block.
 		{
 			check();
 			
-		}catch(ArithmeticException e)
+		}
+		catch(ArithmeticException e)//to catch exception and handle it.
 		{
 			System.out.println("Caught "+e);
 		}
-		
-
-
 	}
-
 }
