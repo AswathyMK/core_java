a899763787874791eaea90e8a387d54308d7490d
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 09:55:08 +0530

    Program for unboxing

--- UnBoxing.java
+++ UnBoxing.java
@@ -1,12 +1,10 @@
-
-public class UnBoxing {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//@SuppressWarnings("deprecation")
+public class UnBoxing 
+{
+	public static void main(String[] args) 
+	{
 		Integer a=new Integer(10);
-		int i=a.intValue();//converting integer to int
-		int j=i;//unboxing , now compiler will write a.intValue() internally
+		int i=a.intValue(); //converting integer to int
+		int j=i;  //unboxing , now compiler will write a.intValue() internally
 		System.out.println(a+" "+i+" "+j);
 
 	}
