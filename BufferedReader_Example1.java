8389b64e2608d4a48c604897e4a3cb766a6ca23d
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 09:15:18 +0530

    Example program for BufferedReader.

--- BufferedReader_Example1.java
+++ BufferedReader_Example1.java
@@ -1,21 +1,17 @@
 import java.io.*;
-public class BufferedReader_Example1 {
-
+public class BufferedReader_Example1 
+{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try
 		{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		float a=Float.parseFloat(br.readLine());
-		double b=Double.parseDouble(br.readLine());
-		System.out.println(a +" \n"+ b);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));//creating BufferedReader object
+		float a=Float.parseFloat(br.readLine());//Reading a number of float type
+		double b=Double.parseDouble(br.readLine());//Reading a number of Double type
+		System.out.println(a +" \n"+ b);//Return float number and double number
 		}
 		catch(Exception e)
 		{
 		
 		}
-		
-
 	}
-
 }
